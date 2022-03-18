4e42c90dd40925a54645a64e3a4c83c4d1f0c621
PhilipH3ro <PhilipH3ro@gmail.com>
2022-03-18 23:53:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var navList = document.getElementById("nav-lists");
+function Show() {
+    navList.classList.add("Menus-show");
+}
+
+function Hide() {
+    navList.classList.remove("Menus-show");
+}
+
+//test
